bb82ba61009bd33b863910b1f0fd1cd70dc6e382
Jeffrey Carandang <jeffreycarandang@Jeffreys-MacBook-Pro.local>
2018-08-12 15:18:51 +0800

    Add Block Options on Widget Options Settings sidebar

--- includes/admin/settings/modules/sidebar-more_plugins.php
+++ includes/admin/settings/modules/sidebar-more_plugins.php
@@ -19,42 +19,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 if( !function_exists( 'widgetopts_settings_more_plugins' ) ):
 	function widgetopts_settings_more_plugins(){ ?>
 		<div id="widgetopts-sidebar-widget-more_plugins" class="postbox widgetopts-sidebar-widget">
-			<h3 class="hndle ui-sortable-handle"><span><?php _e( 'More Free Plugins', 'widget-options' );?></span></h3>
+			<h3 class="hndle ui-sortable-handle"><span><?php _e( 'Ready for Gutenberg?', 'widget-options' );?></span></h3>
 			<div class="inside">
-				<table class="form-table">
-					<tbody>
-						<tr valign="top">
-							<td scope="row">
-								<a href="https://wordpress.org/plugins/forty-four/" target="_blank">
-									<span class="dashicons dashicons-editor-unlink"></span><br />
-									<?php _e( 'Forty Four - 404 Page and 301 SEO Redirection Plugin', 'widget-options' );?>
-								</a>
-							</td>
-							<td scope="row">
-								<a href="https://wordpress.org/plugins/easy-profile-widget/" target="_blank">
-									<span class="dashicons dashicons-admin-users"></span><br />
-									<?php _e( 'Easy Profile Info Widget Plugin', 'widget-options' );?>
-								</a>
-							</td>
-						</tr>
-						<tr valign="top">
-							<td scope="row">
-								<a href="https://wordpress.org/plugins/wp-image-hover-lite/" target="_blank">
-									<span class="dashicons dashicons-share"></span><br />
-									<?php _e( 'Social Icons Image Hover', 'widget-options' );?>
-								</a>
-							</td>
-							<td scope="row">
-								<a href="https://wordpress.org/plugins/wp-author-box-lite/" target="_blank">
-									<span class="dashicons dashicons-nametag"></span><br />
-									<?php _e( 'Author Box Plugin', 'widget-options' );?>
-								</a>
-							</td>
-						</tr>
-					</tbody>
-				</table>
+				<p><?php _e( 'Get the same controls for your Gutenberg blocks using our brand new plugin Block Options for Gutenberg. Also available for free on plugin repository.', 'widget-options' );?></p>
 				<p>
-					<a class="button-secondary" href="https://profiles.wordpress.org/phpbits/#content-plugins" target="_blank"><?php _e( 'View all Free Plugins', 'widget-options' );?></a>
+					<a class="button-secondary" href="https://wordpress.org/plugins/block-options/" target="_blank"><?php _e( 'Download Block Options', 'widget-options' );?></a>
 				</p>
 			</div>
 		</div>
