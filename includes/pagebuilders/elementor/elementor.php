f0c7318b8762509305b2186801fa45616caf901b
Jeffrey Carandang <jeffreycarandang@Jeffreys-MacBook-Pro.local>
2018-10-12 09:03:00 -0700

    Fix issue with ACF fields on Elementor integration

--- includes/pagebuilders/elementor/elementor.php
+++ includes/pagebuilders/elementor/elementor.php
@@ -387,7 +387,7 @@ if( !function_exists( 'widgetopts_elementor_tab_settings' ) ){
         if( isset( $widget_options['acf'] ) && 'activate' == $widget_options['acf'] ){
             $fields = array();
 
-            if ( defined( 'ACF_PRO' ) ) {
+            if ( function_exists( 'acf_get_field_groups' ) ) {
                 $groups = acf_get_field_groups();
                 if ( is_array( $groups ) ) {
                     foreach ( $groups as $group ) {
