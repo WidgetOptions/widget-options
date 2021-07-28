8f47dec2d7fb1fb45cd66554d78d61ed770c29a5
Akshay Paghdar <akshay@justhyre.com>
2021-07-28 10:58:45 +0530

    Version Bump 3.7.9

--- includes/admin/notices.php
+++ includes/admin/notices.php
@@ -38,13 +38,18 @@ if( !function_exists( 'widgetopts_admin_notices' ) ):
         $diff_intrval   = round( ($datetime2->format( 'U' ) - $datetime1->format( 'U' ) ) / (60*60*24));
         if( 'yes' != $saved && $diff_intrval >= 7 ){
         echo '<div class="widgetopts_notice updated" style="box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);">
-            <p>Awesome, you\'ve been using <strong>Widget Options</strong> for more than 1 week. <br> May i ask you to give it a <strong>5-star rating</strong> on WordPress? </br>
-            This will help to spread its popularity and to make this plugin a better one.
-            <br><br>Your help is much appreciated. Thank you very much,<br> ~ Widget Options Team
+            <p>Well done! You have been enjoying <strong>Widget Options</strong> for more than a week. 
+            <br> 
+            Do you love it? Are you over the moon? Will you give us a <a href="https://wordpress.org/support/view/plugin-reviews/widget-options" class="thankyou" target="_blank" title="Ok, you deserved it" style="font-weight:bold;"><strong>5-star rating</strong></a> on WordPress? 
+            </br>
+            Your review is essential to the Widget Options community and our ongoing succes.
+            <br><br>
+            Thank you so much! – Your Widget Options Team
             <ul>
-                <li><a href="https://wordpress.org/support/view/plugin-reviews/widget-options" class="thankyou" target="_blank" title="Ok, you deserved it" style="font-weight:bold;">'. __( 'Ok, you deserved it', 'widget-options' ) .'</a></li>
-                <li><a href="javascript:void(0);" class="widgetopts_bHideRating" title="I already did" style="font-weight:bold;">'. __( 'I already did', 'widget-options' ) .'</a></li>
-                <li><a href="javascript:void(0);" class="widgetopts_bHideRating" title="No, not good enough" style="font-weight:bold;">'. __( 'No, not good enough, i do not like to rate it!', 'widget-options' ) .'</a></li>
+                <li><a href="https://wordpress.org/support/view/plugin-reviews/widget-options" class="thankyou" target="_blank" title="Ok, you deserved it" style="font-weight:bold;">'. __( 'Definitely. Widget Options is the best!', 'widget-options' ) .'</a></li>
+                <li><a href="javascript:void(0);" class="widgetopts_bHideRating" title="I already did" style="font-weight:bold;">'. __( 'Already done!', 'widget-options' ) .'</a></li>
+                <li><a href="https://widget-options.com/contact/" class="thankyou" target="_blank" title="Ok, you deserved it" style="font-weight:bold;">'. __( "I’m not convinced yet. Still thinking about it.", 'widget-options' ) .'</a></li>
+                <li><a href="javascript:void(0);" class="widgetopts_bHideRating" title="No, not good enough" style="font-weight:bold;">'. __( "I don’t do ratings", 'widget-options' ) .'</a></li>
             </ul>
         </div>
         <script>
